847e6253f4c4eacfe53abec7bf64026a15c15b2d
jstanislawczyk <jakubstanislawczyk98@gmail.com>
2024-12-30 09:56:21 +0100

    Refactored frontend

--- apps/frontend/src/App.tsx
+++ apps/frontend/src/App.tsx
@@ -1,10 +1,20 @@
-import {ChangeEvent, useState} from 'react'
+import React, {ChangeEvent, useState} from 'react'
 import './App.css'
 import {useMutation, useQuery} from '@tanstack/react-query';
 import {type AudioJobDto} from '@audio-processor/schemas';
 
+const baseApiURL = 'API_GW/audio-processor/api'; // REPLACE WITH YOUR API GW URL
+
 function App() {
-  const baseApiURL = 'API_GW/audio-processor/api'; // REPLACE WITH YOUR API GW URL
+  return (
+    <div className="audio">
+      <AudioUpload />
+      <UploadedFiles />
+    </div>
+  )
+}
+
+const AudioUpload: React.FC = () => {
   const [file, setFile] = useState<File | null>(null);
   const [errorMessage, setErrorMessage] = useState<string | null>(null);
 
@@ -36,15 +46,6 @@ function App() {
     }
   });
 
-  const fetchAudioJobs = useQuery({
-    queryKey: ['getAudioFiles'],
-    queryFn: async () => {
-      const apiURL = `${baseApiURL}/jobs`;
-      const response = await fetch(apiURL);
-      return response.json();
-    }
-  });
-
   const handleFileChange = (event: ChangeEvent<HTMLInputElement>) => {
     if (event.target.files) {
       setFile(event.target.files[0]);
@@ -61,52 +62,68 @@ function App() {
   };
 
   return (
-    <div className="audio">
-      <div className="audio-upload">
-        {file ? (
-          <section className="file-details">
-            <h2>File details</h2>
-            <ul>
-              <li>Name: {file.name}</li>
-              <li>Type: {file.type}</li>
-              <li>Size: {file.size.toLocaleString()} bytes</li>
-            </ul>
-            <button onClick={handleUpload}>Upload a file</button>
-          </section>
-        ) : (
-          <div className="file-details">
-            <h1>Upload audio file</h1>
-            <input type="file" accept="audio/*,.mp3" onChange={handleFileChange}/>
-          </div>
-        )}
+    <div className="audio-upload">
+      {file ? (
+        <section className="file-details">
+          <h2>File details</h2>
+          <ul>
+            <li>Name: {file.name}</li>
+            <li>Type: {file.type}</li>
+            <li>Size: {file.size.toLocaleString()} bytes</li>
+          </ul>
+          <button onClick={handleUpload}>Upload a file</button>
+        </section>
+      ) : (
+        <div className="file-details">
+          <h1>Upload audio file</h1>
+          <input type="file" accept="audio/*,.mp3" onChange={handleFileChange}/>
+        </div>
+      )}
 
-        {uploadAudioFile.isPending && <p>Uploading...</p>}
-        {uploadAudioFile.isSuccess && <p className="file-upload-success">File successfully uploaded</p>}
-        {errorMessage && <p className="file-upload-error">{errorMessage}</p>}
-      </div>
-      <div className="audio-files">
-        <h1>Uploaded files</h1>
-        {fetchAudioJobs.data ? (
-          <table className="audio-files-table">
-            <tr>
-              <th>File name</th>
-              <th>Created at</th>
-              <th>Status</th>
+      {uploadAudioFile.isPending && <p>Uploading...</p>}
+      {uploadAudioFile.isSuccess && <p className="file-upload-success">File successfully uploaded</p>}
+      {errorMessage && <p className="file-upload-error">{errorMessage}</p>}
+    </div>
+  )
+}
+
+const UploadedFiles: React.FC = () => {
+  const fetchAudioJobs = useQuery({
+    queryKey: ['getAudioFiles'],
+    queryFn: async () => {
+      const apiURL = `${baseApiURL}/jobs`;
+      const response = await fetch(apiURL);
+      return response.json();
+    }
+  });
+
+  return (
+    <div className="audio-files">
+      <h1>Uploaded files</h1>
+      {fetchAudioJobs?.data?.length > 0 ? (
+        <table className="audio-files-table">
+          <thead>
+          <tr>
+            <th>File key</th>
+            <th>Created at</th>
+            <th>Status</th>
+          </tr>
+          </thead>
+          <tbody>
+          {fetchAudioJobs.data.map((job: AudioJobDto) => (
+            <tr key={job.id}>
+              <td>{job.fileName}</td>
+              <td>{new Date(job.createdAt).toLocaleString()}</td>
+              <td className={`job-status-${job.status.toLowerCase()}`}>{job.status}</td>
             </tr>
-            {fetchAudioJobs.data.map((job: AudioJobDto) => (
-              <tr key={job.id}>
-                <td>{job.fileName}</td>
-                <td>{new Date(job.createdAt).toLocaleString()}</td>
-                <td className={`job-status-${job.status.toLowerCase()}`}>{job.status}</td>
-              </tr>
-            ))}
-          </table>
-        ) : (
-          <p>No files uploaded</p>
-        )}
-        {fetchAudioJobs.isLoading && <p>Loading...</p>}
-        <button onClick={() => fetchAudioJobs.refetch()}>Refresh</button>
-      </div>
+          ))}
+          </tbody>
+        </table>
+      ) : (
+        <p>No files uploaded</p>
+      )}
+      {fetchAudioJobs.isLoading && <p>Loading...</p>}
+      <button onClick={() => fetchAudioJobs.refetch()}>Refresh</button>
     </div>
   )
 }
